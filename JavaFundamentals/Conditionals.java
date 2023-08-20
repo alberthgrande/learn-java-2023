9374551cb87d53ab4676efe17ac15148570604fa
alberthgrande <31713127+alberthgrande@users.noreply.github.com>
2023-08-20 22:07:14 +0800

    Added Conditionals

---
+++ JavaFundamentals/Conditionals.java
@@ -0,0 +1,17 @@
+package JavaFundamentals;
+
+public class Conditionals {
+    public static void main(String[] args) {
+        System.out.println("Conditionals");
+
+        JavaFundamentals jFundamentals = new JavaFundamentals();
+        jFundamentals.ifConditionals();
+        jFundamentals.ifElseConditionals();
+        jFundamentals.leadYear();
+        jFundamentals.ternaryOperator();
+        jFundamentals.ifElseIfCondition();
+        jFundamentals.positiveNegativeZero();
+        jFundamentals.nestedIf();
+    }
+
+}
--- JavaFundamentals/JavaFundamentals.java
+++ JavaFundamentals/JavaFundamentals.java
@@ -1,5 +1,7 @@
 package JavaFundamentals;
 
+import java.util.Scanner;
+
 public class JavaFundamentals {
     public void basicSyntax() {
 
@@ -76,5 +78,99 @@ public class JavaFundamentals {
         System.out.println("\ndouble converted to byte");
         x = (byte)b;
         System.out.println("b and x " + b + " " + x);
+    }
+
+    public void ifConditionals() {
+        System.out.println("if condition:");
+        int age = 20;
+        if (age > 18) {
+            System.out.println("Age is greater than " + age);
+        }
+        System.out.println();
+    }
+
+    public void ifElseConditionals() {
+        System.out.println("if-else condition:");
+        int number = 13;
+        if(number % 2 == 0) {
+            System.out.println("even number");
+        }else {
+            System.out.println("odd number");
+        }
+        System.out.println();
+    }
+
+    public void leadYear() {
+        System.out.println("Leap Year:");
+        int year = 2020;
+        if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println("Leap Year");
+        } else {
+            System.out.println("Common Year");
+        }
+        System.out.println();
+    }
+
+    public void ternaryOperator() {
+        System.out.println("Ternary Operator");
+        int divisible = 13;
+        String ouput = (divisible % 2 == 0) ? "even number" : "odd number";
+        System.out.println(ouput);
+
+        System.out.println();
+    }
+
+    public void ifElseIfCondition() {
+        System.out.println("if-else-if condition:");
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Input grade: ");
+        int grade = 100;
+
+        if (grade < 50) {
+            System.out.println("fail!");
+        } else if (grade >= 50 && grade < 60) {
+            System.out.println("D grade");
+        } else if (grade >= 60 && grade < 70) {
+            System.out.println("C grade");
+        } else if (grade >= 70 && grade < 80) {
+            System.out.println("B grade");
+        } else if (grade >= 80 && grade < 90) {
+            System.out.println("A grade");
+        } else if (grade >= 90 && grade <= 100) {
+            System.out.println("A+ grade");
+        } else {
+            System.out.println("invalid!");
+        }
+        System.out.println();
+    }
+
+    public void positiveNegativeZero() {
+        System.out.println("Positive, Negative, or Zero:");
+        int number = 100;
+        if (number > 0) {
+            System.out.println("Positive");
+        } else if (number < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+        System.out.println();
+    }
+
+    public void nestedIf() {
+        System.out.println("Nested if statement:");
+        int age = 20;
+        int weight = 60;
+
+        if (age >= 18) {
+            if (weight > 66) {
+                System.out.println("You are eligible to donate blood.");
+            } else {
+                System.out.println("You are not eligible to donate blood.");
+            }
+        } else {
+            System.out.println("Your age must be greater than 18.");
+        }
+        System.out.println();
     }
 }
