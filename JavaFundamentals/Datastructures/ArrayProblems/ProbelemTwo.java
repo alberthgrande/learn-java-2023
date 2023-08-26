215443c166385cc9b5d41f379eb70ab81047bef0
alberthgrande <31713127+alberthgrande@users.noreply.github.com>
2023-08-26 23:53:41 +0800

    Added Reverse Array

--- JavaFundamentals/Datastructures/ArrayProblems/ProbelemTwo.java
+++ JavaFundamentals/Datastructures/ArrayProblems/ProbelemTwo.java
@@ -1,4 +1,28 @@
 package JavaFundamentals.Datastructures.ArrayProblems;
 
 public class ProbelemTwo {
+
+    public static void reverseArray(int arr[], int start, int end) {
+        int temp;
+         while(start < end) {
+             temp = arr[start];
+             arr[start] = arr[end];
+             arr[end] = temp;
+             start++;
+             end--;
+         }
+    }
+    public static void printArray(int arr[], int size) {
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6};
+        printArray(arr, 6);
+        reverseArray(arr, 0, 5);
+        System.out.println("Reversed Array: ");
+        printArray(arr, 6);
+    }
 }
