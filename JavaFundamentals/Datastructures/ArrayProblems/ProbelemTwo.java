43c0ef0353b835a588f1c3855dd519ae2cdc27d7
alberthgrande <31713127+alberthgrande@users.noreply.github.com>
2023-08-27 23:10:59 +0800

    Added Left Rotate Array

--- JavaFundamentals/Datastructures/ArrayProblems/ProbelemTwo.java
+++ JavaFundamentals/Datastructures/ArrayProblems/ProbelemTwo.java
@@ -2,6 +2,59 @@ package JavaFundamentals.Datastructures.ArrayProblems;
 
 public class ProbelemTwo {
 
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6};
+        printArray(arr, 6);
+        reverseArray(arr, 0, 5);
+        System.out.println("Reversed Array: ");
+        printArray(arr, 6);
+        System.out.println("\n");
+        printRecursiveArray(arr, 6);
+        System.out.println("Reversed Recursive Array: ");
+        recursiveArray(arr, 0, 5);
+        printRecursiveArray(arr, 6);
+
+        System.out.println("\n");
+        int arrLength = arr.length;
+        int display = 2;
+        printRotateArray(arr, arrLength);
+        System.out.println("Rotated Array: ");
+        rotateArray(arr, display, arrLength);
+        printRotateArray(arr, arrLength);
+
+
+    }
+
+    public static void rotateArray(int arr[], int d, int n) {
+        // storing array
+        int[] temp = new int[n];
+        int k = 0;
+
+        // store the current array and n - d
+        for (int i = d; i < n; i++) {
+            temp[k] = arr[i];
+            k++;
+        }
+
+        // store the pop element
+        for (int i = 0; i < d; i++) {
+            temp[k] = arr[i];
+            k++;
+        }
+
+        // copy and print the rotated array
+        for (int i = 0; i < n; i++) {
+            arr[i] = temp[i];
+        }
+    }
+
+    public static void printRotateArray(int arr[], int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
     public static void reverseArray(int arr[], int start, int end) {
         int temp;
          while(start < end) {
@@ -17,19 +70,6 @@ public class ProbelemTwo {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
-    }
-    public static void main(String[] args) {
-        int arr[] = {1,2,3,4,5,6};
-        printArray(arr, 6);
-        reverseArray(arr, 0, 5);
-        System.out.println("Reversed Array: ");
-        printArray(arr, 6);
-        System.out.println("\n");
-        printRecursiveArray(arr, 6);
-        System.out.println("Reversed Recursive Array: ");
-        recursiveArray(arr, 0, 5);
-        printRecursiveArray(arr, 6);
-
     }
 
     public static void recursiveArray(int arr[], int start, int end) {
