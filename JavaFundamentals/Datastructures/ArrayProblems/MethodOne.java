6de87405062133216bbf20a14413a20101afdcae
alberthgrande <31713127+alberthgrande@users.noreply.github.com>
2023-08-31 23:13:48 +0800

    Added Method One in Data Structure

---
+++ JavaFundamentals/Datastructures/ArrayProblems/MethodOne.java
@@ -0,0 +1,33 @@
+package JavaFundamentals.Datastructures.ArrayProblems;
+
+public class MethodOne {
+    public static void main(String[] args) {
+        int arr[] = { 1, 3, 5, 7, 9, 11 };
+        int length = arr.length;;
+        int rotate = 3;
+        rotate = rotate % length;
+        int i,j,k;
+        // reverse the last rotate number
+        for (i = length - rotate, j = length - 1; i < j; i++, j--) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+        // Reverse the first length-rotate terms
+        for (i = 0, j = length - rotate - 1; i < j; i++, j--) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+        // Reverse the entire array
+        for (i = 0, j = length - 1; i < j; i++, j-- ){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+        // print the reverse array
+        for (k = 0; k < length; k++) {
+            System.out.print(arr[k] + " ");
+        }
+    }
+}
