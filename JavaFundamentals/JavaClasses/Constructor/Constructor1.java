b5aedf3a65d60759c0e02f7fea6b514308d2eb3c
alberthgrande <31713127+alberthgrande@users.noreply.github.com>
2023-09-06 23:34:21 +0800

    Added Sample Constructor

---
+++ JavaFundamentals/JavaClasses/Constructor/Constructor1.java
@@ -0,0 +1,13 @@
+package JavaFundamentals.JavaClasses.Constructor;
+
+class Sample1 {
+    Sample1() {
+        System.out.println("Sample1 Constructor");
+    }
+}
+public class Constructor1 {
+    public static void main(String[] args) {
+        Sample1 sample1 = new Sample1();
+
+    }
+}
