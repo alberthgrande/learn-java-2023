1f289974e938567f9fe18add5a22ef10275b0682
alberthgrande <31713127+alberthgrande@users.noreply.github.com>
2023-09-10 16:16:44 +0800

    Added Projects

---
+++ Projects/movies/movies/src/test/java/dev/alberth/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alberth.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
