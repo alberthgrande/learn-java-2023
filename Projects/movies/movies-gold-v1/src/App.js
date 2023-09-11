f304f4503db14464ca207506cf12f10213f7c7bc
alberthgrande <31713127+alberthgrande@users.noreply.github.com>
2023-09-11 20:51:22 +0800

    Fixed api connection to react

--- Projects/movies/movies-gold-v1/src/App.js
+++ Projects/movies/movies-gold-v1/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import api from "./api/axiosConfig";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    try {
+      const response = await api.get("/api/v1/movies");
+      console.log(response.data);
+      setMovies(response.data);
+    } catch (error) {
+      console.error(error); // Use "console.error" instead of "console.log" for errors
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Movie List</h1>
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.imdbId}>{movie.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
