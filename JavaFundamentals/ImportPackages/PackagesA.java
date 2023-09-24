7503e9a86f8a43e4557de3ee7b463cdcfdf220a2
alberthgrande <31713127+alberthgrande@users.noreply.github.com>
2023-09-24 19:30:40 +0800

    Added some packages example

--- JavaFundamentals/ImportPackages/PackagesA.java
+++ JavaFundamentals/ImportPackages/PackagesA.java
@@ -5,4 +5,8 @@ public class PackagesA {
     public void helloWorld() {
         System.out.println("Hello, World!");
     }
+    public void qualifiedName() {
+        System.out.println("Example of package by import fully qualified name");
+    }
+
 }
--- JavaFundamentals/Packages/Packages.java
+++ JavaFundamentals/Packages/Packages.java
@@ -5,5 +5,11 @@ public class Packages {
 
         PackagesA packagesA = new PackagesA();
         packagesA.helloWorld();
+
+        // Example of package by import fully qualified name
+        JavaFundamentals.ImportPackages.PackagesA objA = new JavaFundamentals.ImportPackages.PackagesA();
+        objA.qualifiedName();
+
+
     }
 }
