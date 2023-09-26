7b6d80e9ba2ec3fd72df5a9b2c8f37748d82180f
alberthgrande <31713127+alberthgrande@users.noreply.github.com>
2023-09-26 20:20:17 +0800

    Added Java LinkedList in Data Structure

---
+++ JavaFundamentals/Datastructures/JavaLinkedList/JavaLinkedList.java
@@ -0,0 +1,21 @@
+package JavaFundamentals.Datastructures.JavaLinkedList;
+
+import java.util.LinkedList;
+
+public class JavaLinkedList {
+    public static void main(String[] args) {
+        LinkedList<String> name = new LinkedList<String>();
+        name.add("Alberth");
+        System.out.println(name);
+
+        /*
+        * Method
+        * addFirst()
+        * addLast()
+        * removeFirst()
+        * removeLast()
+        * getFirst()
+        * getLast()
+        * */
+    }
+}
